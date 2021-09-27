b9459618f2e7c44607188065b12760c96ffad61a
leehojin-93 <hogenie93@gmail.com>
2021-09-27 17:33:04 +0900

    210927 themeVo 호진04

---
+++ src/main/java/com/otb/vo/ThemeVo.java
@@ -0,0 +1,39 @@
+package com.otb.vo;
+
+public class ThemeVo {
+	
+	private int themeNo;
+	private String themeName;
+	
+	public ThemeVo() {
+		super();
+	}
+
+	public ThemeVo(int themeNo, String themeName) {
+		super();
+		this.themeNo = themeNo;
+		this.themeName = themeName;
+	}
+
+	public int getThemeNo() {
+		return themeNo;
+	}
+
+	public void setThemeNo(int themeNo) {
+		this.themeNo = themeNo;
+	}
+
+	public String getThemeName() {
+		return themeName;
+	}
+
+	public void setThemeName(String themeName) {
+		this.themeName = themeName;
+	}
+
+	@Override
+	public String toString() {
+		return "ThemeVo [themeNo=" + themeNo + ", themeName=" + themeName + "]";
+	}
+	
+}

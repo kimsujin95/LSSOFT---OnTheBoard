c484b8c688ba0afe2dfd3622f7c68cb2ffa340e1
JaeYeong-Jang <xkxh96@gmail.com>
2021-10-13 14:58:24 +0900

    10/13_reservation_장재영

--- src/main/java/com/otb/controller/ReservationController.java
+++ src/main/java/com/otb/controller/ReservationController.java
@@ -1,10 +1,10 @@
 package com.otb.controller;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,12 +25,20 @@ public class ReservationController {
 	public String reservationinfo(@PathVariable("storeNo")int storeNo, @RequestParam ("checkedTime[]") String[] checkedTime, @RequestParam("matchingNo") int matchingNo) {
 		System.out.println("[reservationController.reservationinfo]");
 		
+		//받아온 데이터 출력
 		for(int i = 0; i < checkedTime.length; i++)  {
-			System.out.println("checkedTime" + checkedTime[i]);
+			System.out.println("checkedTime" + "[" + i + "] : " + checkedTime[i]);
 		}
-		
-		System.out.println(matchingNo);
+		System.out.println("매칭_그룹번호 : " + matchingNo);
 		System.out.println("매장번호 : " + storeNo);
+		
+		Map<String, Object> revselinfo = new HashMap<String, Object>();
+		
+		revselinfo.put("storeNo", storeNo);
+		revselinfo.put("checkedTime", checkedTime);
+		revselinfo.put("matchingNo", matchingNo);
+		
+		reservationService.getreservationinfo(revselinfo);
 		
 		return "1";
 	}

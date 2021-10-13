250f5acc509a1e0e5cabe7aec4354a7d88aa855a
김민수 <minsu.kim1988@gmail.com>
2021-10-13 12:41:58 +0900

    13/Oct/2021 Minsu- GameVo 찜횟수 관련 추가

--- src/main/java/com/otb/vo/GameVo.java
+++ src/main/java/com/otb/vo/GameVo.java
@@ -14,18 +14,20 @@ public class GameVo {
 	private String gameDescription;
 	private String gameGuide;
 	private int themeNo;
+	private String themeName;
 	
 	private String gameThumbImg;
+	private int hitCount;
 	//constructor
 
 	
 
 	public GameVo() {
 	}
-	
+
 	public GameVo(int gameNo, String gameNameKo, String gameNameEn, String gameImg, String gamePeople, String gameAge,
 			String gameTime, String gameDifficulty, String gameDescription, String gameGuide, int themeNo,
-			String gameThumbImg) {
+			String themeName, String gameThumbImg, int hitCount) {
 		super();
 		this.gameNo = gameNo;
 		this.gameNameKo = gameNameKo;
@@ -38,8 +40,11 @@ public class GameVo {
 		this.gameDescription = gameDescription;
 		this.gameGuide = gameGuide;
 		this.themeNo = themeNo;
+		this.themeName = themeName;
 		this.gameThumbImg = gameThumbImg;
+		this.hitCount = hitCount;
 	}
+
 
 	public int getGameNo() {
 		return gameNo;
@@ -49,92 +54,157 @@ public class GameVo {
 		this.gameNo = gameNo;
 	}
 
+
+
 	public String getGameNameKo() {
 		return gameNameKo;
 	}
+
+
 
 	public void setGameNameKo(String gameNameKo) {
 		this.gameNameKo = gameNameKo;
 	}
 
+
+
 	public String getGameNameEn() {
 		return gameNameEn;
 	}
+
+
 
 	public void setGameNameEn(String gameNameEn) {
 		this.gameNameEn = gameNameEn;
 	}
 
+
+
 	public String getGameImg() {
 		return gameImg;
 	}
+
+
 
 	public void setGameImg(String gameImg) {
 		this.gameImg = gameImg;
 	}
 
+
+
 	public String getGamePeople() {
 		return gamePeople;
 	}
+
+
 
 	public void setGamePeople(String gamePeople) {
 		this.gamePeople = gamePeople;
 	}
 
+
+
 	public String getGameAge() {
 		return gameAge;
 	}
+
+
 
 	public void setGameAge(String gameAge) {
 		this.gameAge = gameAge;
 	}
 
+
+
 	public String getGameTime() {
 		return gameTime;
 	}
+
+
 
 	public void setGameTime(String gameTime) {
 		this.gameTime = gameTime;
 	}
 
+
+
 	public String getGameDifficulty() {
 		return gameDifficulty;
 	}
+
+
 
 	public void setGameDifficulty(String gameDifficulty) {
 		this.gameDifficulty = gameDifficulty;
 	}
 
+
+
 	public String getGameDescription() {
 		return gameDescription;
 	}
+
+
 
 	public void setGameDescription(String gameDescription) {
 		this.gameDescription = gameDescription;
 	}
 
+
+
 	public String getGameGuide() {
 		return gameGuide;
 	}
+
+
 
 	public void setGameGuide(String gameGuide) {
 		this.gameGuide = gameGuide;
 	}
 
+
+
 	public int getThemeNo() {
 		return themeNo;
 	}
+
+
 
 	public void setThemeNo(int themeNo) {
 		this.themeNo = themeNo;
 	}
 
+
+
+	public String getThemeName() {
+		return themeName;
+	}
+
+
+
+	public void setThemeName(String themeName) {
+		this.themeName = themeName;
+	}
+
+
+
 	public String getGameThumbImg() {
 		return gameThumbImg;
 	}
 
+
+
 	public void setGameThumbImg(String gameThumbImg) {
 		this.gameThumbImg = gameThumbImg;
+	}
+
+
+	public int getHitCount() {
+		return hitCount;
+	}
+
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
 	}
 
 	@Override
@@ -142,10 +212,11 @@ public class GameVo {
 		return "GameVo [gameNo=" + gameNo + ", gameNameKo=" + gameNameKo + ", gameNameEn=" + gameNameEn + ", gameImg="
 				+ gameImg + ", gamePeople=" + gamePeople + ", gameAge=" + gameAge + ", gameTime=" + gameTime
 				+ ", gameDifficulty=" + gameDifficulty + ", gameDescription=" + gameDescription + ", gameGuide="
-				+ gameGuide + ", themeNo=" + themeNo + ", gameThumbImg=" + gameThumbImg + "]";
+				+ gameGuide + ", themeNo=" + themeNo + ", themeName=" + themeName + ", gameThumbImg=" + gameThumbImg
+				+ ", hitCount=" + hitCount + "]";
 	}
 	
-	
+
 	
 	
 

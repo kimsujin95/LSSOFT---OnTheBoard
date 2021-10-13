cab603834e0ce6fe9b48950f0060a9c7c2349dc1
김민수 <minsu.kim1988@gmail.com>
2021-10-13 15:31:15 +0900

    13/10/2021 Minsu

--- otb_seq,insert.sql
+++ otb_seq,insert.sql
@@ -1,7 +1,6 @@
 -- SEQUENCE
 DROP SEQUENCE seq_users_no;
 DROP SEQUENCE seq_game_no;
-DROP SEQUENCE seq_game_bookmark_no;
 DROP SEQUENCE seq_theme_no;
 DROP SEQUENCE seq_matching_no;
 DROP SEQUENCE seq_sido_no;
@@ -16,11 +15,6 @@ INCREMENT BY 1
 NOCACHE;
 
 CREATE SEQUENCE seq_game_no
-START WITH 1
-INCREMENT BY 1
-NOCACHE;
-
-CREATE SEQUENCE seq_game_bookmark_no
 START WITH 1
 INCREMENT BY 1
 NOCACHE;
@@ -121,7 +115,6 @@ VALUES
         1,
         sysdate
     );
--- // INSERT USERS
 
 -- INSERT GAME
 INSERT INTO 
@@ -253,8 +246,60 @@ VALUES (
 VALUES (
         SEQ_THEME_NO.nextval
     ,   '주사위');
-
 -- // INSERT THEME
+
+-- INSERT GAME
+INSERT INTO 
+    game
+VALUES
+    (
+        seq_game_no.nextval,
+        '보난자',
+        'Bonanza',
+        'upload\thumb\1.png',
+        '3명~5명',
+        '만 10세이상',
+        '45분',
+        '초급',
+        'upload\desc\1.jpg',
+        'OwgKvBQ7bqs',
+        2
+    );
+
+INSERT INTO 
+    game
+VALUES
+    (
+        seq_game_no.nextval,
+        '아컴호러',
+        'Arkham Horror',
+        'upload\thumb\2.png',
+        '1명~4명',
+        '만 12세이상',
+        '60분',
+        '고급',
+        'upload\desc\2.jpg',
+        'OkaaG0_oAtI',
+        7
+    );
+   
+INSERT INTO 
+    game
+VALUES
+    (
+        seq_game_no.nextval,
+        '할리갈리',
+        'Halli Galli',
+        'upload\thumb\3.png',
+        '2명~6명',
+        '만 6세이상',
+        '10분',
+        '초급',
+        'upload\desc\3.jpg',
+        '0q0yClzjADk',
+        5
+    );
+-- // INSERT GAME
 
 -- INSERT SIDO
 INSERT INTO
@@ -472,4 +517,4 @@ values(SEQ_RESERVATION_TIME_NO.nextval, 2, '09');
 
 insert into reservation_time
 values(SEQ_RESERVATION_TIME_NO.nextval, 2, '10');
---// INSERT RESERVATIONDATE
+-- // INSERT RESERVATIONDATE

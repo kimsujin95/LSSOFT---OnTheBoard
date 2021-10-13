f3c6ebf06e0a9bcdb2278ef5b6e0277f0f0b5573
leehojin-93 <hogenie93@gmail.com>
2021-10-13 16:46:27 +0900

    211013 matching 호진01

--- src/main/java/com/otb/sevice/MatchingService.java
+++ src/main/java/com/otb/sevice/MatchingService.java
@@ -7,8 +7,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.otb.dao.MatchingDao;
 import com.otb.dao.UserDao;
